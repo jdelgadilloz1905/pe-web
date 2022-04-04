3e7e96aaeb5dde8612faf23e895e30b6a45ebb1f
Amilcar Barahona <amilcar.laniakea@gmail.com>
2022-04-04 15:31:44 -0400

    agregado video al home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -21,49 +21,35 @@ import './style.sass'
 export default function Home() {
 	return (
 		<>
-			<MetaDescription
-				title={'Home | Wix.com'}
-				name={'description'}
-				content={'Home | Wix.com...'}
-			/>
+			<MetaDescription title={'Home | Wix.com'} name={'description'} content={'Home | Wix.com...'} />
 			<div className='cw-home-banner-global-container'>
+				<iframe
+					className='cw-home-banner-global-video-container'
+					width='1280'
+					height='720'
+					src='https://www.youtube.com/embed/0ucO9frQFmo?mute=1&autoplay=1&modestbranding=1&loop=1&rel=0&amp;controls=0&amp;showinfo=0&playlist=0ucO9frQFmo'
+					title='YouTube EP'
+					frameBorder='0'
+					allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+					allowFullScreen></iframe>
+
 				<div className='cw-home-banner-main-container'>
 					<div className='cw-home-banner-logo-container'>
-						<ScrollAnimation
-							animateIn='animate__fadeInDown'
-							delay={500}
-							animateOnce={true}>
-							<Image
-								classImg={'cw-home-banner-logo-img'}
-								image={logoColor}
-								alt={'Main Logo'}
-								title={'Main Logo'}
-							/>
+						<ScrollAnimation animateIn='animate__fadeInDown' delay={500} animateOnce={true}>
+							<Image classImg={'cw-home-banner-logo-img'} image={logoColor} alt={'Main Logo'} title={'Main Logo'} />
 						</ScrollAnimation>
 					</div>
-					<ScrollAnimation
-						animateIn='animate__fadeInUp'
-						delay={1000}
-						animateOnce={true}>
+					<ScrollAnimation animateIn='animate__fadeInUp' delay={1000} animateOnce={true}>
 						<div className='cw-home-banner-title-container'>
-							<h2 className='cw-home-banner-title'>
-								Hi, it's great to meet you!
-							</h2>
+							<h2 className='cw-home-banner-title'>Hi, it's great to meet you!</h2>
 						</div>
 					</ScrollAnimation>
-					<ScrollAnimation
-						animateIn='animate__fadeInUp'
-						delay={1500}
-						animateOnce={true}>
+					<ScrollAnimation animateIn='animate__fadeInUp' delay={1500} animateOnce={true}>
 						<div className='cw-home-banner-button-container'>
-							<Link
-								to={'/client/step-one'}
-								className='cw-home-banner-first-button'>
+							<Link to={'/client/step-one'} className='cw-home-banner-first-button'>
 								Join as a professional
 							</Link>
-							<Link
-								to={'/wiz-welcome/step-one'}
-								className='cw-home-banner-second-button'>
+							<Link to={'/wiz-welcome/step-one'} className='cw-home-banner-second-button'>
 								Connect with a professional
 							</Link>
 						</div>
