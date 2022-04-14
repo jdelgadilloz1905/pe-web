28491a8a7fcebe2fd9e07628e2e08f398cd3eda2
Amilcar Barahona <amilcar.laniakea@gmail.com>
2022-04-14 16:32:00 -0400

    fixes for recurrent animation in gif next prev button

--- src/pages/wizard/components/Buttons/index.js
+++ src/pages/wizard/components/Buttons/index.js
@@ -1,20 +1,29 @@
 /** @format */
 
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 import animationGif from '../../../../assets/images/icons/Logo-animation-5.gif'
 
 import './style.sass'
 
 export default function Buttons(props) {
+	let location = useLocation()
+	const [isAmimationGif, setAnimationGif] = useState(null)
+	useEffect(() => {
+		setAnimationGif(animationGif)
+
+		return () => {
+			setAnimationGif(null)
+		}
+	}, [location.pathname])
 	return (
 		<>
 			<Link to={props.previous} className='cw-wizard-button'>
 				Back
 			</Link>
 			<div className='cw-wizard-button-icon-container'>
-				<img className='cw-wizard-button-icon' src={animationGif} alt='PE gif' />
+				<img className='cw-wizard-button-icon' src={isAmimationGif} alt='PE gif' />
 			</div>
 			<Link to={props.next} className='cw-wizard-button'>
 				Next
